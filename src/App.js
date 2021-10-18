d9ab97d2ce8aa7f8ca1a81e55417698698f0603f
dvital1903 <72948488+dvital1903@users.noreply.github.com>
2021-10-18 16:10:28 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1> did i do it right?</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <button>hello</button>
+    </div>
+  );
+}
