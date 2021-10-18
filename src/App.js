f0c3c28e18002d35fd6c1238246ac272b68c516f
dvital1903 <72948488+dvital1903@users.noreply.github.com>
2021-10-18 16:45:57 -0500

    2022 uicccp

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1> did i do it right?</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>i am a senior at UICCP</h2>
       <button>hello</button>
     </div>
   );
